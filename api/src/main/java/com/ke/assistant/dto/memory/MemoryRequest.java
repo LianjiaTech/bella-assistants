2cbba6bfbf665527cd6cace29eff0867ad24b25b
saizhuolin001 <saizhuolin001@ke.com>
2025-09-16 15:42:44 +0800

    feat: update json field map

--- api/src/main/java/com/ke/assistant/dto/memory/MemoryRequest.java
+++ api/src/main/java/com/ke/assistant/dto/memory/MemoryRequest.java
@@ -1,5 +1,6 @@
 package com.ke.assistant.dto.memory;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ke.assistant.enums.MemoryType;
 import lombok.Data;
 
@@ -10,13 +11,14 @@ public class MemoryRequest {
 
     private String user;
 
+    @JsonProperty("thread_id")
     private String threadId;
     
     private String query;
     
     private MemoryType type = MemoryType.MIX_MEMORY;
     
-    @Valid
+    @JsonProperty("strategy_param")
     private StrategyParam strategyParam = new StrategyParam();
     
     public boolean invalidQuery() {
