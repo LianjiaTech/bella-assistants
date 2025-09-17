9b9b39463af9a0f6b0c612fc1a473e3719dea156
saizhuolin001 <saizhuolin001@ke.com>
2025-09-17 10:38:42 +0800

    feat: swagger authorization

--- api/src/main/java/com/ke/assistant/configuration/ApiDocConfiguration.java
+++ api/src/main/java/com/ke/assistant/configuration/ApiDocConfiguration.java
@@ -1,8 +1,11 @@
 package com.ke.assistant.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +24,12 @@ public class ApiDocConfiguration {
                         .version("1.0.0")
                         .license(new License()
                                 .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")));
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                .addSecuritySchemes("Authorization", new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")));
     }
 }
